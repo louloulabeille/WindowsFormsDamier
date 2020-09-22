1ac63ffd2ba932125c9b9c7567e4d783239ae7ba
louloulabeille <louloulabeille@hotmail.com>
2020-09-22 21:50:53 +0200

    Presque clope

--- WindowsFormsDamier/FormDamier.Designer.cs
+++ WindowsFormsDamier/FormDamier.Designer.cs
@@ -48,11 +48,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(37, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(239, 17);
+            this.label1.Size = new System.Drawing.Size(109, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Taille du damier en nombre de ligne:";
+            this.label1.Text = "Taille du damier";
             // 
             // button1
             // 
@@ -118,7 +118,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(30, 72);
+            this.label3.Location = new System.Drawing.Point(33, 72);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(116, 17);
             this.label3.TabIndex = 10;
